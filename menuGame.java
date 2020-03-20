68983210b595a2eae49baa32f732c547a9592c9b
Lê Bá Đạt <61006100+lebadat1996@users.noreply.github.com>
2020-03-20 17:02:54 +0700

    Add files via upload

--- menuGame.java
+++ menuGame.java
@@ -8,16 +8,19 @@ public class menuGame {
         int Selection = -1;
         while (Selection != 0) {
             System.out.println("menu");
-            System.out.println("1. Draw the triangle");
+            System.out.println("1. Draw the triangle top left");
             System.out.println("2. Draw the square");
             System.out.println("3. Draw the rectangle");
+            System.out.println("4. Draw the triangle BotTon-left");
+            System.out.println("5. Draw the triangle BotTon-right");
+            System.out.println("6. Draw the triangle top right");
             System.out.println("0. Exit");
-            System.out.println("Enter your choice: ");
+            System.out.print("Enter your choice: ");
             Selection = scanner.nextInt();
 
             switch (Selection) {
                 case 1: {
-                    System.out.println("Draw the triangle");
+                    System.out.println("Draw the triangle top left");
                     for (int i = 7; i > 0; i--) {
                         for (int j = 1; j < i; j++) {
                             System.out.print(" * ");
@@ -47,6 +50,46 @@ public class menuGame {
                     }
                     break;
                 }
+                case 4: {
+                    System.out.println("Draw the triangle BotTon-left");
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print(" * ");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                }
+                case 5: {
+                    System.out.println("Draw the triangle BotTon-right");
+                    for (int i = 1; i <= 5; i++) {
+                        for (int j = 1; j <= 5; j++) {
+                            if (j <= 5 - i) {
+                                System.out.print("   ");
+                            } else {
+                                System.out.print("*  ");
+
+                            }
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                }
+                case 6: {
+                    System.out.println("Draw the triangle top right");
+                    for (int i = 0; i <= 5; i++){
+                        for(int j = 0; j <= i; j++){
+                            System.out.print(" ");
+                        }
+                        for(int j = 0; j <= 5 - i; j++){
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+
+                }
+                break;
+
                 case 0:
                     System.exit(0);
                 default:
